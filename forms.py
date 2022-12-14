698bfde8069a1f28019cf68f9e04849984b6ce8b
Cody Gula <cgula7@gmail.com>
2022-12-13 19:03:29 -0800

    Going back to origional DB for login

--- forms.py
+++ forms.py
@@ -47,10 +47,10 @@ class register_form(FlaskForm):
     )
 
 
-    def validate_email(self, email):
-        if User.query.filter_by(email=email.data).first():
-            raise ValidationError("Email already registered!")
+    # def validate_email(self, email):
+    #     if User.query.filter_by(email=email.data).first():
+    #         raise ValidationError("Email already registered!")
 
-    def validate_uname(self, uname):
-        if User.query.filter_by(username=username.data).first():
-            raise ValidationError("Username already taken!")
+    # def validate_uname(self, uname):
+    #     if User.query.filter_by(username=username.data).first():
+    #         raise ValidationError("Username already taken!")
--- routes.py
+++ routes.py
@@ -4,8 +4,7 @@ from flask import (
     redirect,
     flash,
     url_for,
-    session,
-    request
+    session
 )
 
 from datetime import timedelta
@@ -17,9 +16,7 @@ from sqlalchemy.exc import (
     InvalidRequestError,
 )
 from werkzeug.routing import BuildError
-import json
-import scratchpad
-import boto3
+
 
 from flask_bcrypt import Bcrypt,generate_password_hash, check_password_hash
 
@@ -36,11 +33,6 @@ from app import create_app,db,login_manager,bcrypt
 from models import User
 from forms import login_form,register_form
 
-dbclient = boto3.resource("dynamodb")
-searchdatabase = dbclient.Table("searchItems")
-AuthTable = dbclient.Table("email_list")
-
-
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -55,57 +47,31 @@ def session_handler():
 
 @app.route("/", methods=("GET", "POST"), strict_slashes=False)
 def index():
-    resp = scratchpad.query_table('searchItems', 'email', '222222')
-    posts = resp.get('Items')
-    return render_template("index.html",title="Home", posts=posts)
+    return render_template("index.html",title="Home")
 
 
 @app.route("/login/", methods=("GET", "POST"), strict_slashes=False)
 def login():
-    print("login called")
     form = login_form()
 
     if form.validate_on_submit():
-        print('form.validate_on_submit() called')
         try:
-            print('try thing entered')
-            user = form.email.data
-            password = form.pwd.data
-            print(user)
-            response = AuthTable.get_item(
-                TableName="email_list",
-                Key={
-                    "email": user
-                })
-            
-            
-            dbpassword = response['Item']['passwordhash']
-            print('dbpassword = ', dbpassword)
-            print('password = ', password)
-
-
-            if check_password_hash(dbpassword, password):
-                print("logging in!!!!!!!!!!!!")
-                resp = scratchpad.query_table('searchItems', 'email', '222222')
-                posts = resp.get('Items')
-                print("No errors before login_user !!!!!!!!!!!!")
-               
-                print("No errors after login_user !!!!!!!!!!!!")
-                return render_template('dashboard.html', posts=posts, user=user)
-
+            user = User.query.filter_by(email=form.email.data).first()
+            if check_password_hash(user.pwd, form.pwd.data):
+                login_user(user)
+                return redirect(url_for('index'))
             else:
-                print("check pasword failed")
                 flash("Invalid Username or password!", "danger")
         except Exception as e:
             flash(e, "danger")
-            print("except thing happened")
-    print('Right before return render_template()')
+
     return render_template("auth.html",
         form=form,
         text="Login",
         title="Login",
         btn_action="Login"
         )
+
 
 
 # Register route
@@ -117,24 +83,13 @@ def register():
             email = form.email.data
             pwd = form.pwd.data
             username = form.username.data
-            # put dynamoDB stuff here?
+            
             newuser = User(
                 username=username,
                 email=email,
                 pwd=bcrypt.generate_password_hash(pwd),
             )
-            ############################# This works
-            pwdhash=bcrypt.generate_password_hash(pwd).decode('utf-8')
-            response = AuthTable.put_item(
-                Item={
-                    "email": email,
-                    "password": pwd,
-                    "passwordhash": pwdhash,
-                    })
-            print(response) 
-
-
-            #############################
+    
             db.session.add(newuser)
             db.session.commit()
             flash(f"Account Succesfully created", "success")
@@ -170,56 +125,6 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for('login'))
-
-
-
-@app.route('/DeleteItem',  methods =["POST"])
-def DeleteItem():
-    information = request.data
-    deleteThis = int(information.decode("utf-8"))
-    # replace with email from login
-    email = '222222'
-    response = scratchpad.delete_info(email, deleteThis)
-    print(response)
-   
-    resp = scratchpad.query_table('searchItems', 'email', '222222')
-    posts = resp.get('Items')
-    return render_template('index.html', posts=posts)
-
-@app.route('/TestItem', methods = ["POST"])
-def TestItem():
-    information = request.data
-    # information = request.form
-    testThis = int(information.decode("utf-8"))
-
-    # insert code to send a test email
-    print('TestItem() called!!!! testThis= ', testThis)
-
-    resp = scratchpad.query_table('searchItems', 'email', '222222')
-    posts = resp.get('Items')
-    return render_template('index.html', posts=posts)
- 
-
-@app.route('/AddItem', methods=["GET","POST"])
-def AddItem():
-    print("AddItem() Called")
-
-    information = request.data
-    testThis = (information.decode("utf-8"))
-    data = json.loads(testThis)
-
-    print("testThis= ", testThis)
-    print("AddItem() data[item] = ", data["item"])
-    print("AddItem() data[URL] = ", data["URL"])
-
-    response = scratchpad.put_info('222222', data["URL"], data["item"])
-    print(response)
-
-
-    resp = scratchpad.query_table('searchItems', 'email', '222222')
-    posts = resp.get('Items')
-    
-    return render_template('index.html', posts=posts)
 
 
 if __name__ == "__main__":
